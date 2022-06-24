263bf44bced2b84ac606253b48416fd5e07593ff
kttheman1 <104694182+kttheman1@users.noreply.github.com>
2022-06-23 20:33:05 -0400

    updates

--- src/test/java/apps/bjs/SearchTest.java
+++ src/test/java/apps/bjs/SearchTest.java
@@ -5,20 +5,20 @@ import base.CommonAPI;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.ebay.HomePage;
-import pages.ebay.SearchResultPage;
+import pages.bjs.HomePage;
+import pages.bjs.SearchResultPage;
 
 public class SearchTest extends CommonAPI {
 
-    @Test
-    public void searchBarWorks() {
-    HomePage homePage = new HomePage(getDriver());
-    homePage.clickSearchBar();
-    homePage.typeSearchTerm("eggs");
-    homePage.clickSearchBtn();
-    Assert.assertTrue(getDriver().findElement(By.xpath("//*[@id=\"contentOverlay\"]/div/app-search-main/app-search-result-page-gb/div[2]/app-search-header/div/span/div")).isDisplayed());
-
-}
+//    @Test
+//    public void searchBarWorks() {
+//    HomePage homePage = new HomePage(getDriver());
+//    homePage.clickSearchBar();
+//    homePage.typeSearchTerm("eggs");
+//    homePage.clickSearchBtn();
+//    Assert.assertTrue(getDriver().findElement(By.xpath("//*[@id='contentOverlay']/div/app-search-main/app-search-result-page-gb/div[2]/app-search-header/div/span/div")).isDisplayed());
+//
+//}
 
     @Test
     public void searchLandOLakesEggs() {
@@ -27,7 +27,7 @@ public class SearchTest extends CommonAPI {
     homePage.clickSearchBar();
     homePage.typeSearchTerm("Land O Lakes eggs");
     homePage.clickSearchBtn();
-    Assert.assertTrue(getDriver().findElement(By.cssSelector("mz-thumb-selected dark-bg mz-thumb")).isDisplayed());
+    Assert.assertTrue(getDriver().findElement(By.xpath("//*[@id='slot3']/div[3]/div/app-pdp-name-and-item-number-organism/section/div[3]/div/div[2]/div/span")).isDisplayed());
 
 }
 
